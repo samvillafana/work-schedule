33fe31a324f2280ea60f830399b46fe5c6d42fa3
Samuel <samuel@samuelvafanamac.myfiosgateway.com>
2022-10-30 22:10:26 -0400

    finished logic to update localStorage and change colors based off of current hour base on JS

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ const getCurrentHour = () => {
 $(document).ready(function () {
     $('#currentDay').text(moment().format("dddd, MMMM Do"))
 
-    // when document loads we are going grab all taks by hour from local storage and populate
+    // when document loads we are going grab all tasks by hour from local storage and populate
     let hourNine = JSON.parse(localStorage.getItem('hour9')) || '';
     let hourTen = JSON.parse(localStorage.getItem('hour10')) || '';
     let hourEleven = JSON.parse(localStorage.getItem('hour11')) || '';
@@ -15,6 +15,7 @@ $(document).ready(function () {
     let hourThree = JSON.parse(localStorage.getItem('hour3')) || '';
     let hourFour = JSON.parse(localStorage.getItem('hour4')) || '';
     let hourFive = JSON.parse(localStorage.getItem('hour5')) || '';
+
     // Fill in the respective <textarea> tags with the description
     $('.text-area-9').text(hourNine.desc ? hourNine.desc : '')
     $('.text-area-10').text(hourTen.desc ? hourTen.desc : '')
@@ -25,6 +26,34 @@ $(document).ready(function () {
     $('.text-area-3').text(hourThree.desc ? hourThree.desc : '')
     $('.text-area-4').text(hourFour.desc ? hourFour.desc : '')
     $('.text-area-5').text(hourFive.desc ? hourFive.desc : '')
+
+
+    const currentHour = moment().hour();
+    // loop thru the time slots and grab their hour by the id and compare it to the current hour to determine color.
+    $(".time-block").each(function (obj, i) {
+        let timeBlockTime = parseInt(i.id.split('hour')[1]);
+
+        console.log(i.id.split('hour')[1])
+        switch (true) {
+            case timeBlockTime === currentHour:
+                $(this).removeClass('past')
+                $(this).removeClass('future')
+                $(this).addClass('current')
+                break
+            case timeBlockTime < currentHour:
+                $(this).removeClass('current')
+                $(this).removeClass('future')
+                $(this).addClass('past')
+                break
+            case timeBlockTime > currentHour:
+                $(this).removeClass('present')
+                $(this).removeClass('past')
+                $(this).addClass('future')
+                break
+        }
+    })
+
+
 
     // adding an event listener for all the save buttons to capture their respective text area .siblings() did not work.
     $(".saveBtn-9").on('click', () => {
